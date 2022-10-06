000303c81652080a346a74996ba28b1cf75233b0
ilgibitum <ilgibitum@192.168.1.11>
2022-10-06 10:27:58 +0500

    2 digits

--- square_root.py
+++ square_root.py
@@ -1,4 +1,4 @@
 num = float(input('Enter a number: '))
 
 num_sqrt = num ** 0.5
-print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+print('The square root of %0.2f is %0.2f'%(num ,num_sqrt))
